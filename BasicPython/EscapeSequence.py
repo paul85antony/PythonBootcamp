6dfed91476a10d951536569c6a920e023adbef1c
Paul Antony <paulantony@Mac-1328.lan>
2026-08-23 22:57:40 -0500

    sequence using backslash

---
+++ BasicPython/EscapeSequence.py
@@ -0,0 +1,13 @@
+print("Hello \"world\" ") #to add double quotes in the string
+print('Hellu "world" ') #to add double quotes in the string
+#to use the real backslash in python we have to add \\
+print("path file C:\\bin\\test")
+
+
+
+#to create a new empty line
+print("Hello world\n") #to create a new empty line
+print("after the empty line")
+
+#to create a tab space
+print("Hello\tworld") #to create a tab space
